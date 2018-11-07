892bbebb344760e04e6b23fda66c77a8838d2fbe
hapi666 <281127500@qq.com>
2018-11-07 23:58:39 +0800

    解决了昨天的疑惑

--- practice38/practice38.go
+++ practice38/practice38.go
@@ -25,6 +25,14 @@ type Port struct {
 	StrVal string
 }
 
+/*
+*之前我一直特别疑惑，因为这是我从别人博客里看到的嘛，
+*我不太理解：为什么实现了这个Unmarshaller接口里面的UnmarshalJSON()方法
+*就是实现了能够自己预想的 反序列化/序列化 呢
+*第二天。。。
+*我翻阅源码（之前粗略翻阅并没有发现），忽然看到了在json包里面，在反序列化之前是要判断这个v的值类型的
+ */
+
 // 实现 json.Unmarshaller 接口
 func (port *Port) UnmarshalJSON(value []byte) error {
 	if value[0] == '"' {
